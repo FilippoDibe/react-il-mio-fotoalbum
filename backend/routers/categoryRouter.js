4493f14e5275f178f769e7bab33e595947bfdcf3
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-06-27 12:03:44 +0200

    add protect rotte

--- backend/routers/categoryRouter.js
+++ backend/routers/categoryRouter.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const validator = require ("../middlewares/validator.js")
 const { categoryData } = require ("../validations/categoryValidation.js")
 const {create, index, destroy} = require ("../controllers/categoryController.js")
+const protect = require ("../middlewares/authMiddleware.js")
 
-router.post("/",validator(categoryData), create)
-router.get("/", index)
-router.delete("/:id", destroy)
+
+router.post("/",validator(categoryData),protect, create)
+router.get("/",protect, index)
+router.delete("/:id",protect, destroy)
 
 module.exports = router;
--- backend/routers/photoRouter.js
+++ backend/routers/photoRouter.js
@@ -4,14 +4,15 @@ const validator = require ("../middlewares/validator.js")
 const {photoData} = require("../validations/photoValidation.js")
 const {create, index, update, showBySlug, destroy} = require ("../controllers/photoController.js");
 const uniqueSlug = require ("../middlewares/uniqueSlug.js");
+const protect = require ("../middlewares/authMiddleware.js")
 
 
 
 router.get("/", index)
 router.get("/:slug", showBySlug)
-router.post("/",validator(photoData), uniqueSlug, create)
-router.put("/:slug",validator(photoData), update)
-router.delete("/:slug", destroy)
+router.post("/",validator(photoData), protect, uniqueSlug, create)
+router.put("/:slug",validator(photoData), protect, update)
+router.delete("/:slug",protect, destroy)
 
 
 
